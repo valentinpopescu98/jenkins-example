96a4582572cf48bb95f0c75a0d09322e01c77a8e
Valentin Popescu <67784803+valentinpopescu98@users.noreply.github.com>
2022-10-18 13:44:43 +0300

    Added sudo

--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-docker run -p 8081:8081 -p 50000:50000 -d -v jenkins_home:/var/jenkins_home -e JENKINS_OPTS="--httpPort=8081" jenkins/jenkins:lts
+sudo docker run -p 8081:8081 -p 50000:50000 -d -v jenkins_home:/var/jenkins_home -e JENKINS_OPTS="--httpPort=8081" jenkins/jenkins:lts
